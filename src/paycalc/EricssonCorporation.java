3300f4474797fff14b79d1f31cf3b32832e3b258
Goldy Liang <goldyliang@gmail.com>
2016-01-04 12:35:01 -0500

    Added comments and JavaDoc. Separate unit tests for success and wrong inputs

--- src/paycalc/EricssonCorporation.java
+++ src/paycalc/EricssonCorporation.java
@@ -1,5 +1,20 @@
 package paycalc;
 
+/**
+ * A simple class for the service of calculating per-week payment, according to the following rules:
+ * 
+ *     For hours within {@value #HOURS_THREASHOLD_EXTRA_PAY}, pay by base hourly salary. 
+ *     For hours over {@value #HOURS_THREASHOLD_EXTRA_PAY}, pay by {@value #TIMES_EXTRA_PAY} times of base hourly salary
+ * 
+ * Following conditions are considered error, and error code is to be returned.
+ *    > Hourly base salary less than minimum which is {@value #MIN_PAY_PER_HOUR}.
+ *    > Total working hour exceeding maximum which is {@value #MAX_HOURS_PER_WEEK}.
+ *    
+ * Limitation: if both error conditions are met, only the error code for the prior error is returned.
+ *    
+ * @author goldyliang@gmail.com
+ *
+ */
 public class EricssonCorporation {
 	
 	private static final float MIN_PAY_PER_HOUR = 8.0f;
@@ -7,25 +22,72 @@ public class EricssonCorporation {
 	private static final float TIMES_EXTRA_PAY = 1.5f;
 	private static final int MAX_HOURS_PER_WEEK = 60;
 	
+	/**
+	 * Status codes for the payment calculation, indicating valid result or error.
+	 */
 	public enum CalcStatus {
+		/**
+		 * Accepted input and valid result returned
+		 */
 		ACCEPTED, 
-		TOO_MANY_HOURS, 
+		
+		/**
+		 * Input with working hours exceeding limit. Payment calculation returned anyway.
+		 */
+		TOO_MANY_HOURS,
+		
+		/**
+		 * Input with base salary under minimum. Payment calculation returned anyway.
+		 */
 		TOO_LOW_BASE_SALARY,
+		
+		/**
+		 * Invalid input. No payment calculation returned.
+		 */
 		INVALID_INPUT};
 	
+	/**
+	 * Class to wrap the calculation status and result
+	 */
 	public static class CalcResult {
 		private CalcStatus status;
 		private float totalPay;
 		
+		// This class is only for returning the result
+		// So public constructor disallowed.
 		private CalcResult (CalcStatus status, float total) {
 			this.status = status;
 			this.totalPay = total;
 		}
-				
-		public CalcStatus getStatus () {return status;}
-		public float getTotalPay () {return totalPay;}
+		
+		/** 
+		 * Get status of calculation
+		 * @return Status code. See {@link CalcStatus} for detail definition
+		 */
+		public CalcStatus getStatus () {
+			return status;
+		}
+		
+		/**
+		 * Get calculated total payment (in dollars)
+		 * @return Total payment for the week
+		 */
+		public float getTotalPay () {
+			return totalPay;
+		}
 	}
 	
+	/**
+	 * Calculate and return the total payment for the week.
+	 * The calculation status is returned along with the calculated total payment
+	 * through the wrapper class {@link CalcResult}
+	 * 
+	 * See {@link EricssonCorporation} for detail rules of calculation.
+	 * 
+	 * @param basePay Base hourly salary (in dollars)
+	 * @param hours Hours of working in the week
+	 * @return Calculation result for the weekly payment, wrapped with a status code
+	 */
 	public static CalcResult calcWeeklyPay (float basePay, int hours) {
 		
 		if (basePay <= 0 || hours <= 0)
@@ -48,11 +110,15 @@ public class EricssonCorporation {
 		return new CalcResult (status, pay);
 	}
 	
+	// Helper method printing the header for calculation result (testing purpose only)
+	// Ideally this can be put in a separate package
 	static void printHeader() {
 		System.out.println(String.format("%1$10s %2$15s %3$15s %4$15s %5$25s",
 				"Employee", "Base Pay", "Hours Worked" ,"Total To Pay", "Error Info"));
 	}
 	
+	// Helper method printing one record of calculation (testing purpose only)
+	// Ideally this can be put in a separate package
 	static void printPayCalc (int order, float basePay, int hours) {
 		
 		CalcResult res = calcWeeklyPay (basePay, hours);
@@ -62,6 +128,8 @@ public class EricssonCorporation {
 				order, basePay, hours, res.totalPay, res.status));
 	}
 	
+	// This is a simple demo of the function.
+	// Ideally this can be put in a separate package
 	public static void main (String[] args) {
 		
 		printHeader();
--- src/paycalctest/TestEricssonCorporation.java
+++ src/paycalctest/TestEricssonCorporation.java
@@ -15,17 +15,40 @@ public class TestEricssonCorporation {
 		
 		CalcResult r;
 		
-		r = EricssonCorporation.calcWeeklyPay(7.5f, 35);
-		assertEquals (CalcStatus.TOO_LOW_BASE_SALARY, r.getStatus());
-		assertEquals ( 7.5f * 35, r.getTotalPay(), 0.005);
+		r = EricssonCorporation.calcWeeklyPay(8.2f, 35);
+		assertEquals (CalcStatus.ACCEPTED, r.getStatus());
+		assertEquals ( 8.2f * 35, r.getTotalPay(), 0.005);
+		
+		r = EricssonCorporation.calcWeeklyPay(8.2f, 40);
+		assertEquals (CalcStatus.ACCEPTED, r.getStatus());
+		assertEquals ( 8.2f * 40, r.getTotalPay(), 0.005);
 		
 		r = EricssonCorporation.calcWeeklyPay(8.2f, 47);
 		assertEquals (CalcStatus.ACCEPTED, r.getStatus());
 		assertEquals ( 8.2f * 40 + 8.2f * 1.5 * 7, r.getTotalPay(), 0.005);
+		
+		r = EricssonCorporation.calcWeeklyPay(8f, 47);
+		assertEquals (CalcStatus.ACCEPTED, r.getStatus());
+		assertEquals ( 8f * 40 + 8f * 1.5 * 7, r.getTotalPay(), 0.005);
+		
+		r = EricssonCorporation.calcWeeklyPay(15f, 60);
+		assertEquals (CalcStatus.ACCEPTED, r.getStatus());
+		assertEquals ( 15f * 40 + 15f * 1.5 * 20, r.getTotalPay(), 0.005);
 
-		r = EricssonCorporation.calcWeeklyPay(10f, 73);
+	}
+	
+	@Test
+	public void testCalcWeeklyPay_Error() {
+		
+		CalcResult r;
+		
+		r = EricssonCorporation.calcWeeklyPay(7.9f, 35);
+		assertEquals (CalcStatus.TOO_LOW_BASE_SALARY, r.getStatus());
+		assertEquals ( 7.9f * 35, r.getTotalPay(), 0.005);
+		
+		r = EricssonCorporation.calcWeeklyPay(10f, 61);
 		assertEquals (CalcStatus.TOO_MANY_HOURS, r.getStatus());
-		assertEquals ( 10f * 40 + 10f * 1.5 * (73-40), r.getTotalPay(), 0.005);
+		assertEquals ( 10f * 40 + 10f * 1.5 * (61-40), r.getTotalPay(), 0.005);
 		
 		r = EricssonCorporation.calcWeeklyPay(7.5f, 70);
 		assertEquals (CalcStatus.TOO_LOW_BASE_SALARY, r.getStatus());
